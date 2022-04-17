3ef5cc254273a8d85cb9866e5d86af64b4d58d69
Quinten Mortier <quinten.mortier@student.kuleuven.be>
2022-04-17 13:59:31 +0200

    quick overview layout done

--- src/app/components/overview/overview.component.ts
+++ src/app/components/overview/overview.component.ts
@@ -1,89 +1,96 @@
 import { Component, OnInit } from '@angular/core';
 import * as d3 from 'd3';
+import { PieArcDatum } from 'd3';
+
+
+// set the dimensions and margins of the graph
+
+
+function calcPercent(percent:number) {
+  return [percent, 100 - percent];
+};
+
+interface Data {
+  quantity: number;
+  category: string;
+}
+
+
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.scss']
 })
-
-// set the dimensions and margins of the graph
-
-
-
-
 export class OverviewComponent implements OnInit {
+
+  //initial dimensions
+  width = 120;
+  height = 120;
+  margin = 40;
+  radius = Math.min(this.width, this.height) / 2 - this.margin;
+
+  
+
+  data: Data[] = [
+    {
+        quantity: 30, //winning percentage
+        category: 'a'
+    },
+    {
+        quantity: 70, //remainder (100 - winning %)
+        category: 'b'
+    }
+];
+
+
+  pie = d3.pie<Data>().sort(null).value((data) => data.quantity)
+
+  data_ready = this.pie(this.data)
+
+
+  color = d3
+        .scaleOrdinal()
+        .domain(
+          (d3.extent(this.data, (d) => {
+            return d.category
+          }) as unknown) as string
+        )
+        .range(["#fe6262", "#c9c9c9"])
+
 
   constructor() { }
 
   ngOnInit(): void {
-
-    // set the dimensions and margins of the graph
-    const width = 50,
-    height = 50,
-    margin = 40;
-
-    // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
-    const radius = Math.min(width, height) / 2 - margin
-
-    // append the svg object to the div called 'my_dataviz'
-    const svg = d3.select("#percentage")
-  .append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("transform", `translate(${width / 2},${height / 2})`);
-
-    interface Data {
-        quantity: number;
-        category: string;
-    }
-
-    // Create dummy data
-    let testData: Data[] = [
-        {
-            quantity: 10,
-            category: 'a'
-        },
-        {
-            quantity: 20,
-            category: 'b'
-        },
-        {
-            quantity: 10,
-            category: 'c'
-        },
-        {
-            quantity: 100,
-            category: 'd'
-        },
-        {
-            quantity: 500,
-            category: 'e'
-        }
-    ];
-
-
-//     // set the color scale
-//     const color = d3.scaleOrdinal()
-//   .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56"])
-    
-//   let pie = d3.pie<Data>().value((d: Data):number => d.quantity);
-
-
-//     const data_ready = pie(Object.entries(data))
+    const svg = d3.select("#my_dataviz")
+    .append("svg")
+      .attr("width", this.width)
+      .attr("height", this.height)
+    .append("g")
+      .attr("transform", `translate(${this.width / 2},${this.height / 2})`);
      
-//     svg
-//   .selectAll('whatever')
-//   .data(data_ready)
-//   .join('path')
-//   .attr('d', d3.arc<d3.Arc<Data>().innerRadius(100).outerRadius(radius)
-//   )
-//   .attr('fill', d => color(d.data[0]))
-//   .attr("stroke", "black")
-//   .style("stroke-width", "2px")
-//   .style("opacity", 0.7)
+    const text = svg.append("text")
+      .attr("text-anchor", "middle")
+      .attr("dy", ".3em")
+      .attr("class", "percentage") 
+      .style("fill","black")
+      .style("font-size","30px")
+      .style("font-weight","bold");
+  
+    text.text(this.data[0].quantity + "%")
+   
+  
+    svg.selectAll('whatever')
+    .data(this.data_ready)
+    .join('path')
+    .attr('d', d3.arc<PieArcDatum<Data>>()
+      .innerRadius(59)         
+      .outerRadius(43) // This is the size of the donut hole
+    )
+    .attr('fill',d => {return this.color(d.data.category) as string})
+    .style("opacity", 0.7)
 
-//   }
+
+    
   }
 }
