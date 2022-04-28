3d9cf578944003e894f9eec6a5a25c15459e5e3e
r0847009 <anton.thomas@student.kuleuven.be>
2022-04-28 11:06:30 +0200

    Updated layout after feedback

--- src/app/components/overview/overview.component.js
+++ src/app/components/overview/overview.component.js
@@ -1,1 +1,1 @@
-d3.select('#RadialProgressBar').selectAll('p').data([1,2,3]).enter().append('p').text(dta => dta);
+// d3.select('#RadialProgressBar').selectAll('p').data([1, 2, 3]).enter().append('p').text(dta => dta);
